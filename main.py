2ed62436e5abf17a021fe4f47dfaac2e8a5875ac
Judith Magee <Judith783@gmail.com>
2021-07-07 21:58:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+user_input = input("Enter a person's name: ")
+
+if user_input=="Mom":
+  print("Birthday is 11/05/40")
+elif user_input=="Dad":
+    print("Birhday is 12/16/37")
+elif user_input=="Brother":
+      print("Birthday is 11/05/20")
+else: 
+  print("Can't find a birthday for this person ")
